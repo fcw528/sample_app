2e9cedf702c28c340c077b750328a2204afb560e
“BlueWhale” <fcw528@gmail.com>
2014-01-10 22:58:47 +0800

    =Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://ruby.taobao.org/'
+ruby '2.0.0'
+
+gem 'rails', '4.0.2'
+gem 'pg', '0.17.1'
+
+group :development, :test do
+  gem 'rspec-rails','2.14.1'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
+end
+
+gem 'sass-rails', '4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.1'
+gem 'jquery-rails', '2.2.1'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+#SampleApp::Application.config.secret_key_base = '98cf112b582278217fa41dfe16e01d0eb92e8799cb690f325235ebd7ebd72981dc1e287af2d3ae921bdd29ea5b4b1452681cecbb5442423281249fb00dc37702'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
