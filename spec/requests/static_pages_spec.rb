b419ec67eadd60839207fd79b3afc6c1981bcfbe
“BlueWhale” <fcw528@gmail.com>
2014-01-11 18:33:02 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 #  describe "GET /static_pages" do
 #    it "works! (now write some real specs)" do
 #      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get static_pages_index_path
+#      get index_path
 #      response.status.should be(200)
 #    end
 #  end
@@ -12,22 +12,14 @@ require 'spec_helper'
 
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
+  let(:base_title) { full_title('') }
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+    before { visit root_path }
+    it { should have_content('Sample App') }
+    #it { should have_title("#{base_title} | Home") }
+    it { should have_title(full_title('')) }
+    it { should have_title(full_title('Home')) }
 
     #it "should not have a custom page title" do
     #  visit '/static_pages/home'
@@ -36,18 +28,11 @@ describe "Static pages" do
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+    before { visit help_path }
+    it { should have_content('Help') }
+    #it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('')) }
+    it { should have_title(full_title('Help')) }
 
     #it "should not have a custom page title" do
     #  visit '/static_pages/home'
@@ -57,18 +42,11 @@ describe "Static pages" do
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+    before { visit about_path }
+    it { should have_content('About Us') }
+    #it { should have_title("#{base_title} | About Us") }
+    it { should have_title(full_title('')) }
+    it { should have_title(full_title('About Us')) }
 
     #it "should not have a custom page title" do
     #  visit '/static_pages/home'
@@ -77,19 +55,10 @@ describe "Static pages" do
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title(full_title('')) }
+    it { should have_title(full_title('Contact')) }
 
     #it "should not have a custom page title" do
     #  visit '/static_pages/home'
