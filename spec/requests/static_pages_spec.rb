075e831410bb2b92901831b98dad9bd9c7208b1c
“BlueWhale” <fcw528@gmail.com>
2014-01-11 21:40:21 +0800

    优化测试代码

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,57 +13,53 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   let(:base_title) { full_title('') }
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
-    #it { should have_title("#{base_title} | Home") }
-    it { should have_title(full_title('')) }
-    it { should have_title(full_title('Home')) }
-
-    #it "should not have a custom page title" do
-    #  visit '/static_pages/home'
-    #  expect(page).not_to have_title('| Home')
-    #end
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help') }
-    #it { should have_title("#{base_title} | Help") }
-    it { should have_title(full_title('')) }
-    it { should have_title(full_title('Help')) }
-
-    #it "should not have a custom page title" do
-    #  visit '/static_pages/home'
-    #  expect(page).not_to have_title('| Help')
-    #end
-
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About Us') }
-    #it { should have_title("#{base_title} | About Us") }
-    it { should have_title(full_title('')) }
-    it { should have_title(full_title('About Us')) }
-
-    #it "should not have a custom page title" do
-    #  visit '/static_pages/home'
-    #  expect(page).not_to have_title('| About Us')
-    #end
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
-    it { should have_title(full_title('')) }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
+    it { should have_selector('h1', text: 'Contact') }
+  end
 
-    #it "should not have a custom page title" do
-    #  visit '/static_pages/home'
-    #  expect(page).not_to have_title('| Contact')
-    #end
-
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Sign in"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
   end
 end
